7af49cfcd008553325da803dc0ecd0a16b59d682
aneagoie <aneagoie1@gmail.com>
2019-09-08 11:56:25 +1200

    lost

---
+++ pdf.py
@@ -0,0 +1,13 @@
+import PyPDF2
+
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+  page = template.getPage(i)
+  page.mergePage(watermark.getPage(0))
+  output.addPage(page)
+
+  with open('watermarked_output.pdf', 'wb') as file:
+    output.write(file)
