364f5b4822d39cd5812684aaa138ad027d56fe8b
Andrei Neagoie <10776230+aneagoie@users.noreply.github.com>
2024-03-12 07:59:04 -0400

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,10 +1,17 @@
 import PyPDF2
 
-template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
-watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
-output = PyPDF2.PdfFileWriter()
+# template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+# watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+# output = PyPDF2.PdfFileWriter()
+# This is the new way to do this:
+template = PyPDF2.PdfReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfWriter()
 
-for i in range(template.getNumPages()):
+
+# for i in range(template.getNumPages()):
+# New way to do this:
+for i in range(len(template.pages)):
   page = template.getPage(i)
   page.mergePage(watermark.getPage(0))
   output.addPage(page)
@@ -12,4 +19,11 @@ for i in range(template.getNumPages()):
   with open('watermarked_output.pdf', 'wb') as file:
     output.write(file)
     
-  
+
+ 
+
+
+ 
+
+
+ 
