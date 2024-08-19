f591e11d1bf9a151e9e13d58ca7933ac43492879
Andrei Neagoie <10776230+aneagoie@users.noreply.github.com>
2024-08-19 14:25:11 -0400

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -12,16 +12,19 @@ output = PyPDF2.PdfWriter()
 # for i in range(template.getNumPages()):
 # New way to do this:
 for i in range(len(template.pages)):
-  page = template.getPage(i)
-  page.mergePage(watermark.getPage(0))
-  output.addPage(page)
-
-  with open('watermarked_output.pdf', 'wb') as file:
-    output.write(file)
+    page = template.pages[i]
+    page.merge_page(watermark.pages[0])
+    output.add_page(page)
+ 
+with open('watermaked_output.pdf', 'wb') as outputFile:
+    output.write(outputFile)
     
 
  
 
+
+ 
+ 
 
  
 
