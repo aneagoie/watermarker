e90ddb2ba09b144415a58c762a8d327d529511f8
Andrei Neagoie <aneagoie@Andreis-MacBook-Pro-2.local>
2025-09-24 08:19:02 -0400

    update to pypdf

--- pdf.py
+++ pdf.py
@@ -1,16 +1,19 @@
-import PyPDF2
+# OLD: import PyPDF2
+# NEW: 
+import pypdf
 
 # template = PyPDF2.PdfFileReader(open('superduper.pdf', 'rb'))
 # watermark = PyPDF2.PdfFileReader(open('water.pdf', 'rb'))
 # output = PyPDF2.PdfFileWriter()
-# This is the new way to do this:
-template = PyPDF2.PdfReader(open('superduper.pdf', 'rb'))
-watermark = PyPDF2.PdfReader(open('water.pdf', 'rb'))
-output = PyPDF2.PdfWriter()
+
+# THIS IS THE NEW WAY TO DO THIS:
+template = pypdf.PdfReader(open('superduper.pdf', 'rb'))
+watermark = pypdf.PdfReader(open('water.pdf', 'rb'))
+output = pypdf.PdfWriter()
 
 
 # for i in range(template.getNumPages()):
-# New way to do this:
+# THIS IS THE NEW WAY TO DO THIS:
 for i in range(len(template.pages)):
     page = template.pages[i]
     page.merge_page(watermark.pages[0])
